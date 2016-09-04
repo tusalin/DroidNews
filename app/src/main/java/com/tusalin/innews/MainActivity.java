d6645e3730cd055c4948f6ed9bb04ea9c2c4e3b6
tusalin <zhanqianlin@gmail.com>
2016-09-04 21:14:24 +0800

    Initial commit,init drawerlayout

---
+++ app/src/main/java/com/tusalin/innews/MainActivity.java
@@ -0,0 +1,90 @@
+package com.tusalin.innews;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+import android.view.View;
+
+import com.tusalin.innews.fragment.EntertainmentFragment;
+import com.tusalin.innews.fragment.HomeFragment;
+import com.tusalin.innews.fragment.TechnologyFragment;
+
+public class MainActivity extends AppCompatActivity {
+
+    private DrawerLayout drawerlayout;
+    private Toolbar toobar;
+    private ActionBarDrawerToggle drawerToggle;
+    private HomeFragment homeFragment;
+    private NavigationView navigationview;
+    private TechnologyFragment technologyFragment;
+    private EntertainmentFragment entertainmentFragment;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.layout_home);
+        navigationview = (NavigationView) findViewById(R.id.navigation_view);
+        drawerlayout = (DrawerLayout) findViewById(R.id.drawerlayout);
+        toobar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toobar);
+        toobar.setNavigationIcon(R.drawable.menu);
+        setupNavigation();
+
+    }
+
+    public void setupNavigation(){
+        drawerToggle = new ActionBarDrawerToggle(this,drawerlayout,toobar,
+                R.string.drawer_open, R.string.drawer_close){
+            @Override
+            public void onDrawerOpened(View drawerView) {
+                super.onDrawerOpened(drawerView);
+            }
+
+            @Override
+            public void onDrawerClosed(View drawerView) {
+                super.onDrawerClosed(drawerView);
+            }
+        };
+        drawerlayout.addDrawerListener(drawerToggle);
+        navigationview.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                FragmentManager fragmentManager = getSupportFragmentManager();
+                FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+                switch (item.getItemId()){
+                    case R.id.menu_home:
+                        homeFragment = new HomeFragment();
+                        fragmentTransaction.replace(R.id.framelaoyout,homeFragment);
+                        toobar.setTitle("Home");
+                        break;
+                    case R.id.menu_technology:
+                        technologyFragment = new TechnologyFragment();
+                        fragmentTransaction.replace(R.id.framelaoyout,technologyFragment);
+                        toobar.setTitle("Technology");
+                        break;
+                    case R.id.menu_entertainment:
+                        entertainmentFragment = new EntertainmentFragment();
+                        fragmentTransaction.replace(R.id.framelaoyout,entertainmentFragment);
+                        toobar.setTitle("Entertainment");
+                        break;
+
+                }
+                fragmentTransaction.commit();
+                item.setChecked(true);
+                drawerlayout.closeDrawers();
+                return true;
+            }
+        });
+    }
+
+
+
+}
---
+++ app/src/main/java/com/tusalin/innews/fragment/HomeFragment.java
@@ -0,0 +1,26 @@
+package com.tusalin.innews.fragment;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.tusalin.innews.R;
+
+/**
+ * Created by tusalin on 9/4/2016.
+ */
+
+public class HomeFragment extends Fragment {
+
+    public HomeFragment(){}
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_home,container,false);
+        return view;
+    }
+}
